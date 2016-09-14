1c690cb668b4bd16302e47b1315cb2ada3021d92
Jared Siirila <siirila@gmail.com>
2016-09-14 00:23:22 -0400

    Add webpack config file

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
 var $ = require('jquery');
 
-$('.categories').append('<p>Test</p>');
+var categories = [
+    'hats',
+    'space',
+    'funny',
+    'ties',
+    'kittens',
+    'sinks',
+    'clothes'
+];
+
+var baseCatUrl = 'http://thecatapi.com/api/images/get?',
+    picSizeParam = 'size=small';
+
+$.each(categories, function(index, category) {
+    // Add a section for this category to the page
+    var $category = $('<section>');
+    $category.appendTo('.categories');
+    $category.append('<header><h1>' + category + '</h1></header>');
+
+    var i = 0;
+    while(i < 5) {
+        var src = baseCatUrl + '&category=' + category + '&rand=' + Math.floor(Math.random() * 500);
+        var $img = $('<img>').attr('src', src).appendTo($category);
+        i++;
+    }
+});
+
